ac2cdecb2d6b0c73c5f36acaf369fbbc3999fa17
Frederick Johnson <fritzj96@gmail.com>
2021-08-02 14:02:37 -0500

    added: logic for creating & displaying notes

--- src/components/CreateNoteForm.js
+++ src/components/CreateNoteForm.js
@@ -1,9 +1,39 @@
+import firebase from "firebase";
 import { useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+const placeholder = `# You can use markdown in this field.
+
+### H3 Heading
+
+*Italic*
+
+**Bold**
+
+etc etc.
+`
+
 export const CreateNoteForm = () => {
+  const [user] = useAuthState(firebase.auth());
   const [noteTitle, setNoteTitle] = useState("");
   const [noteBody, setNoteBody] = useState("");
+
+  const createNote = (e) => {
+    e.preventDefault();
+    if (noteTitle && noteBody) {
+      firebase
+        .firestore()
+        .collection("users")
+        .doc(user?.uid)
+        .collection("notes")
+        .add({ title: noteTitle, body: noteBody, created: new Date() });
+      setNoteTitle("");
+      setNoteBody("");
+    }
+  }
+
   return (
-    <form className="space-y-8 divide-y divide-gray-200">
+    <form onSubmit={createNote} className="space-y-8 divide-y divide-gray-200">
       <div className="space-y-8 divide-y divide-gray-200">
         <div>
           <div>
@@ -16,8 +46,8 @@ export const CreateNoteForm = () => {
               </label>
               <div className="mt-1 rounded-md shadow-sm">
                 <input
-                value={noteTitle}
-                onChange={(e) => setNoteTitle(e.target.value)}
+                  value={noteTitle}
+                  onChange={(e) => setNoteTitle(e.target.value)}
                   type="text"
                   name="noteTitle"
                   id="noteTitle"
@@ -35,17 +65,23 @@ export const CreateNoteForm = () => {
               </label>
               <div className="mt-1">
                 <textarea
-                value={noteBody}
-                onChange={(e) => setNoteBody(e.target.value)}
+                  value={noteBody}
+                  onChange={(e) => setNoteBody(e.target.value)}
                   id="noteDody"
                   name="notebody"
                   rows={10}
                   className="shadow-sm focus:ring-green-500 focus:border-green-500 block w-full p-1 sm:p-2 sm:text-lg border border-gray-500 rounded-md"
                   defaultValue={""}
+                  placeholder={placeholder}
                 />
               </div>
             </div>
-            <button type="submit" className="mt-6 p-4 w-full text-center text-2xl text-white bg-green-500">Save Note</button>
+            <button
+              type="submit"
+              className="mt-6 p-4 w-full text-center text-2xl text-white bg-green-500"
+            >
+              Save Note
+            </button>
           </div>
         </div>
       </div>
--- src/components/NotePanel.js
+++ src/components/NotePanel.js
@@ -3,17 +3,14 @@ import { useCollectionData } from "react-firebase-hooks/firestore";
 
 import { NoteCard } from "./NoteCard";
 
-const body = `#### Test
-
-*lol*`;
 export const NotePanel = ({ uid }) => {
   const [notes] = useCollectionData(
-    firebase.firestore().collection("users").doc(uid).collection("notes"),
+    firebase.firestore().collection("users").doc(uid).collection("notes").orderBy("created", "desc"),
     { idField: "id" }
   );
   if (notes?.length > 0) {
     return (
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-gray-100 py-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-gray-100 py-10 gap-4 sm:gap-6 lg:gap-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
         {notes.map((note) => (
           <NoteCard key={note.id} title={note.title} body={note.body} />
         ))}
